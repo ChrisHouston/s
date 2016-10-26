e946a84a352b08222eaba075ef5958b8bf5ef2bf
Christopher Houston <chris@nabumedia.com>
2016-10-26 18:02:10 +0100

    New features added
    - find optimised to use getElementById where possible
    - find can handle full css selectors
    - find returns all matching subchildren
    - event handling (dispatch, listen, stopListening)
    - data() will set all matching tags when setting.

--- s.js
+++ s.js
@@ -1,3 +1,4 @@
+
 (function() {
 
 	function select(id, parent) {
@@ -44,25 +45,77 @@
 		if (id instanceof HTMLElement) {
 			return new Selector([id]);
 		} else {
-			var type = 0;
 			var els = [];
-			if (id.charAt(0) == "#") {
-				type = 1;
-			} else if (id.charAt(0) == ".") {
-				type=2;
-				id = id.substr(1);
-			}
-			if (type == 1) {
-				var el = parent.querySelector(id);
-				if (el) {
-					els = [el];	
-				}
-			} else if (type == 2) {
-				els = parent.getElementsByClassName(id);
+			var char0 = id.charAt(0);
+			var substr1 = id.substr(1);
+			/* if first character is not alphabetical, . or # OR subsequent characters contain .#*,>+~]:) or space, use querySelectorAll */
+			if (/[^\.#a-z]/.test(char0) || /[\.#\*,\s>\+~\]:\)]/.test(substr1)) {
+				els = parent.querySelectorAll(id);
 			} else {
-				els = parent.getElementsByTagName(id);
+				switch (char0) {
+					case "#":
+						var el = document.getElementById(substr1);
+						if (el) {
+							/* check that this element is in fact a child of the parent */
+							var par = el.parentElement;
+							while (par) {
+								if (par == parent || parent == document) {
+									els = [el];	
+									break;
+								}
+								par = par.parentElement;
+							}
+						}
+						break;
+					case ".":
+						els = parent.getElementsByClassName(id.substr(1));
+						break;
+					default:
+						els = parent.getElementsByTagName(id);
+				}
 			}
 			return new Selector(els);
+		}
+	}
+	
+	select.listen = function (el, eventName, handler) {
+		if (el.addEventListener) {
+			el.addEventListener(eventName, handler, false);
+		} else if (el.attachEvent && htmlEvents['on' + eventName]) { // IE < 9
+			el.attachEvent('on' + eventName, handler);
+		} else {
+			el['on' + eventName] = handler;
+		}
+	}
+
+	select.stopListening = function (el, eventName, handler) {
+		if (el.removeventListener) {
+			el.removeEventListener(eventName, handler, false);
+		} else if (el.detachEvent && htmlEvents['on' + eventName]) { // IE < 9
+			el.detachEvent('on' + eventName, handler);
+		} else {
+			el['on' + eventName] = null;
+		}
+	}
+	
+	select.dispatch = function(el, eventName) {
+		var event;
+		if (document.createEvent) {
+			event = document.createEvent('HTMLEvents');
+			event.initEvent(eventName, true, true);
+		} else if (document.createEventObject) { // IE < 9
+			event = document.createEventObject();
+			event.eventType = eventName;
+		}
+		event.eventName = eventName;
+		if (el.dispatchEvent) {
+			el.dispatchEvent(event);
+		} else if (el.fireEvent && htmlEvents['on' + eventName]) { // IE < 9
+			el.fireEvent('on' + event.eventType, event); // can trigger only real event (e.g. 'click')
+		} else if (el[eventName]) {
+			el[eventName]();
+		} else if (el['on' + eventName]) {
+			el['on' + eventName]();
 		}
 	}
 	
@@ -118,18 +171,49 @@
 		}
 
 		this.data = function (key, value) {
+			/* returns first non-undefined value when getting, but sets all values when setting. */
+			var out, val;
 			for (var i = me.length - 1; i > -1; i--) {
-				var out = select.data(me[i], key, value);
-				if (out != undefined) {
-					return out;
+				val = select.data(me[i], key, value);
+				if (val != undefined) {
+					out = val;
 				}
 			}
-			return undefined;
+			return out;
 		}
 		
 		this.find = function(id) {
-			return select.find(id, me[0]);
+			var els = [];
+			for (var i = 0; i <me.length; i++) {
+				var sel = select.find(id, me[i]);
+				for (var j=0;j<sel.length;j++) {
+					els.push(sel[j]);
+				}
+			}
+			return new Selector(els);
 		}
+		
+		this.listen = function(eventName, handler) {
+			for (var i = me.length - 1; i > -1; i--) {
+				select.listen(me[i], eventName, handler);
+			}
+			return me;
+		}
+		
+		this.stopListening = function(eventName, handler) {
+			for (var i = me.length - 1; i > -1; i--) {
+				select.stopListening(me[i], eventName, handler);
+			}
+			return me;
+		}
+				
+		this.dispatch = function(eventName) {
+			for (var i = me.length - 1; i > -1; i--) {
+				select.dispatch(me[i], eventName);
+			}
+			return me;
+		}
+		
 	}
 
 	window.s = window.s || select;
